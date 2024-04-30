1e400fec049932387a71d8d201da429b0ad14433
Le Nguyen Ngoc <lebkp04@gmail.com>
2024-04-30 23:21:50 +0700

    first commit

---
+++ src/main/java/com/myproject/chatappsimple/ChatAppSimpleApplication.java
@@ -0,0 +1,13 @@
+package com.myproject.chatappsimple;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatAppSimpleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChatAppSimpleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/myproject/chatappsimple/ChatAppSimpleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myproject.chatappsimple;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatAppSimpleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
